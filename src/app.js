6234df82fb6a1b275a054cfd57d21854c38769ca
Lakshya Kumar <lakshyakumar5023@gmail.com>
2025-01-25 21:45:00 +0530

    error-handling

--- src/app.js
+++ src/app.js
@@ -19,6 +19,22 @@ app.get("/user/getData", userAuth, (req, res, next) => {
     res.send("User data available");
 })
 
+app.get("/getUserData", (req, res, next) => {
+    try {
+        throw new Error("Dummy Error");
+        res.send("User data sent successfully");
+    } catch (error) {
+        res.status(500).send("Something went wrong. Please try again later");
+    }
+})
+
+app.use("/", (err, req, res, next) => {
+    if (err) {
+        console.log(err);
+        res.status(500).send("Something went wrong");
+    }
+})
+
 app.listen(3000, () => {
     console.log("Successfully started")
 });
