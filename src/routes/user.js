52a088becd018fe225b1d5c28010fda7eb76c323
Lakshya Kumar <lakshyakumar5023@gmail.com>
2025-02-06 18:47:08 +0530

    feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const ConnectionRequest = require('../models/connectionRequest');
 const { userAuth } = require('../middlewares/auth');
+const User = require('../models/user');
 
 const userRouter = express.Router();
 
 //Get all pending requests 
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
-    try{
+    try {
         const loggedInUser = req.user;
         const connectionRequests = await ConnectionRequest.find({
             toUserId: loggedInUser._id,
             status: "interested",
-        }).populate("fromUserId", "firstName lastName profilePicture gender bio location experienceLevel skills"); 
+        }).populate("fromUserId", "firstName lastName profilePicture gender bio location experienceLevel skills");
 
 
         res.json({
@@ -19,7 +20,7 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
             data: connectionRequests,
         });
     }
-    catch(err){
+    catch (err) {
         res.status(500).json({
             message: "Internal Server Error" + err.message,
         });
@@ -37,22 +38,22 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
                 { toUserId: loggedInUser._id, status: "accepted" },
             ],
         })
-        .populate({
-            path: 'fromUserId',
-            select: DATA
-        })
-        .populate({
-            path: 'toUserId',
-            select: DATA
-        });
+            .populate({
+                path: 'fromUserId',
+                select: DATA
+            })
+            .populate({
+                path: 'toUserId',
+                select: DATA
+            });
 
         // Map the connections to get connected user data
         const connectedUsers = connections.map(connection => {
             // If fromUserId is logged in user, return toUser details, else return fromUser details
-            const connectedUser = connection.fromUserId._id.equals(loggedInUser._id) 
-                ? connection.toUserId 
+            const connectedUser = connection.fromUserId._id.equals(loggedInUser._id)
+                ? connection.toUserId
                 : connection.fromUserId;
-            
+
             // Only include the specified fields
             const { firstName, lastName, profilePicture, gender, bio, location, experienceLevel, skills } = connectedUser;
             return {
@@ -81,5 +82,43 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
         });
     }
 });
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+    //not => his own card
+    //not => already connected
+    //not => already sent request
+    //not => ignored
+    try {
+        const loggedInUser = req.user;
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                { fromUserId: loggedInUser._id },
+                { toUserId: loggedInUser._id }
+            ]
+        }).select("toUserId fromUserId");
+
+        const hideUsersFromFeed = new Set();
+        connectionRequests.forEach((request) => {
+            hideUsersFromFeed.add(request.fromUserId.toString());
+            hideUsersFromFeed.add(request.toUserId.toString());
+        });
+
+        hideUsersFromFeed.add(loggedInUser._id.toString());
+
+        const feedUsers = await User.find({
+            _id: { $nin: Array.from(hideUsersFromFeed) }
+        }).select(DATA);
+
+        res.json({
+            message: "Feed users fetched successfully",
+            data: feedUsers
+        });
+    }
+    catch (err) {
+        res.status(500).json({
+            message: "Internal Server Error: " + err.message,
+        });
+    }
+})
 
 module.exports = userRouter;
