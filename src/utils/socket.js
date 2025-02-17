eb82df3a9d60995b7a30b480d2a2106f45091957
Lakshya Kumar <lakshyakumar5023@gmail.com>
2025-02-17 11:32:33 +0530

    real time chat feature using socket.io

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -9,10 +9,15 @@ const initializeSocket = (server) =>{
     });
 
     io.on("connection", (socket)=>{
-        socket.on("joinChat", () => {
-
+        socket.on("joinChat", ({ userId, targetUserId}) => {
+            const roomId = [userId, targetUserId].sort().join("-");
+            socket.join(roomId);
         });
-        socket.on("sendMessage", () => {
+        socket.on("sendMessage", ({firstName, lastName, senderId, text, targetUserId, timestamp}) => {
+            const roomId = [senderId, targetUserId].sort().join("-");
+            io.to(roomId).emit("receiveMessage", {firstName, lastName, text, timestamp});
+        });
+        socket.on("leaveChat", () => {
             
         });
         socket.on("disconnect", () => {
