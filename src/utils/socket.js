6f30d646353f825a1ca045bcbefeda3e31646419
Lakshya Kumar <lakshyakumar5023@gmail.com>
2025-02-17 12:08:43 +0530

    improved security of room ID

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,4 +1,9 @@
 const socket = require('socket.io');
+const crypto = require('crypto');
+
+const getSecretRoomId = (userId, targetUserId) => {
+    return crypto.createHash("sha256").update([userId, targetUserId].sort().join("$")).digest("hex");
+}
 
 const initializeSocket = (server) =>{
     const io = socket(server, {
@@ -10,11 +15,11 @@ const initializeSocket = (server) =>{
 
     io.on("connection", (socket)=>{
         socket.on("joinChat", ({ userId, targetUserId}) => {
-            const roomId = [userId, targetUserId].sort().join("-");
+            const roomId = getSecretRoomId(userId, targetUserId);
             socket.join(roomId);
         });
         socket.on("sendMessage", ({firstName, lastName, senderId, text, targetUserId, timestamp}) => {
-            const roomId = [senderId, targetUserId].sort().join("-");
+            const roomId = getSecretRoomId(senderId, targetUserId);
             io.to(roomId).emit("receiveMessage", {firstName, lastName, text, timestamp});
         });
         socket.on("leaveChat", () => {
